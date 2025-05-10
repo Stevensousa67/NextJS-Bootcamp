98bcabcd76ae47d5722e0a07a163e20d5e735b8a
Steven Sousa <silver.steven@hotmail.com>
2025-05-09 23:00:32 -0400

    started foodie project

---
+++ foodies-app/app/meals/page.js
@@ -0,0 +1,10 @@
+export default function MealsPage(){
+    return (
+        <main>
+            <h1>Meals</h1>
+            <p>🔥 This is the meals page! 🔥</p>
+            <p>🔥 We are learning about the app directory! 🔥</p>
+            <p>🔥 Let&apos;s get started! 🔥</p> 
+        </main>
+    )
+}
---
+++ foodies-app/app/meals/share/page.js
@@ -0,0 +1,10 @@
+export default function ShareMealPage() {
+  return (
+    <main>
+      <h1>Share your meal</h1>
+      <p>🔥 This is the share page! 🔥</p>
+      <p>🔥 We are learning about the app directory! 🔥</p>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
