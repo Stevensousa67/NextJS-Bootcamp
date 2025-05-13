32239880f91af265c280132d80f22222b99c4d48
Steven Sousa <silver.steven@hotmail.com>
2025-05-13 15:47:32 -0400

    setup output of meals & images with unknown dimensions

--- foodies-app/app/meals/page.js
+++ foodies-app/app/meals/page.js
@@ -1,10 +1,28 @@
-export default function MealsPage(){
-    return (
-        <main>
-            <h1>Meals</h1>
-            <p>🔥 This is the meals page! 🔥</p>
-            <p>🔥 We are learning about the app directory! 🔥</p>
-            <p>🔥 Let&apos;s get started! 🔥</p> 
-        </main>
-    )
+import classes from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import MealsGrid from "@/app/components/meals/meals-grid";
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
